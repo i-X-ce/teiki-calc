e786dbc6f4921062105a264170f53d290fd1c4f1
i-X-ce <iceci2804@gmail.com>
2025-08-23 22:27:27 +0900

    ✨ エラーモーダルを追加

---
+++ src/components/ErrorModal/index.tsx
@@ -0,0 +1,52 @@
+import { Modal, Text } from "@mantine/core";
+import { createContext, useState, type ReactNode } from "react"
+import { MdError } from "react-icons/md";
+
+type ErrorModalContextType = {
+    openError: ({ title, content }: { title: string, content: ReactNode }) => void;
+    closeError: () => void;
+} | null;
+export const ErrorModalContext = createContext<ErrorModalContextType>(null);
+
+const ErrorModalProvider = ({ children }: { children: ReactNode }) => {
+    const [open, setOpen] = useState(false);
+    const [title, setTitle] = useState<string>("");
+    const [content, setContent] = useState<ReactNode>(null);
+    const value = {
+        openError: ({ title, content }: { title: string, content: ReactNode }) => {
+            setOpen(true);
+            setTitle(title);
+            setContent(content);
+        },
+        closeError: () => {
+            setOpen(false);
+        }
+    }
+
+    return (
+        <ErrorModalContext.Provider value={value}>
+            {children}
+            <Modal
+                title={
+                    <Text c={"red"}>
+                        <MdError />
+                        {title}
+                    </Text>
+                }
+                opened={open}
+                onClose={() => value.closeError()}>
+                {content}
+            </Modal>
+        </ErrorModalContext.Provider >
+    )
+}
+
+export const useErrorModal = () => {
+    const context = createContext(ErrorModalContext);
+    if (!context) {
+        throw new Error("useErrorModal must be used within a ErrorModalProvider");
+    }
+    return context;
+}
+
+export default ErrorModalProvider
